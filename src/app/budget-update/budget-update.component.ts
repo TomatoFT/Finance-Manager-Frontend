461d2333f805a1f59052d69b5ffc13706fc85315
tanchauKMS <tanchau@kms-technology.com>
2023-08-22 01:00:58 +0700

    [Fix] Fix the budget_name bug

--- src/app/budget-update/budget-update.component.ts
+++ src/app/budget-update/budget-update.component.ts
@@ -1,43 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BudgetService } from '../budget.service';
-import { Observable, Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-budget-update',
   templateUrl: './budget-update.component.html',
   styleUrls: ['./budget-update.component.css']
 })
-export class BudgetUpdateComponent implements OnInit {
+export class BudgetUpdateComponent implements OnInit, OnDestroy {
   budgetForm!: FormGroup;
-  data: any[] = [];
+  data: any = {}; // Change data type to object
   dataSubscription!: Subscription;
 
-  constructor(private formBuilder: FormBuilder, 
-              public budgetService: BudgetService,   
-              private route: ActivatedRoute) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    public budgetService: BudgetService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    // @Input() id?: Intl;
-    // const id = 1; // Replace with the actual ID you want to retrieve
     this.route.params.subscribe(params => {
-      const id = params['id']; 
-  
-    this.budgetService.getData(id)
-      .subscribe({
-        next: data => {
-          this.data = data;
-          console.log(this.data.values);
+      const id = params['id'];
+
+      this.dataSubscription = this.budgetService.getData(id).subscribe(
+        data => {
+          this.data = data[0]; // Assign the first item in the data array
+          console.log(this.data);
           this.initializeForm();
         },
-        error: error => {
+        error => {
           console.error('Error:', error);
         }
-      });
-  })
-}
+      );
+    });
+  }
 
   ngOnDestroy() {
     if (this.dataSubscription) {
@@ -47,15 +45,30 @@ export class BudgetUpdateComponent implements OnInit {
 
   initializeForm() {
     this.budgetForm = this.formBuilder.group({
-      budget_name: [this.data[0]?.budget_name, Validators.required],
-      income_category_id: [this.data[0]?.income_category_id, Validators.required],
-      user_id: [this.data[0]?.user_id, Validators.required],
-      amount: [this.data[0]?.amount, Validators.required],
-      always_notify: [this.data[0]?.always_notify],
+      budget_name: [this.data.budget_name, Validators.required],
+      income_category_id: [this.data.income_category_id, Validators.required],
+      user_id: [this.data.user_id, Validators.required],
+      amount: [this.data.amount, Validators.required],
+      always_notify: [this.data.always_notify],
     });
   }
 
   onSubmit() {
-    console.log(this.budgetForm.value)
+    console.log(this.budgetForm.value);
+    const id = this.route.snapshot.params['id'];
+    fetch(`http://localhost:8000/budget/update_budget_data/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.budgetForm.value)
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log('Success:', result);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }
 }
