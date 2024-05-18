7177ee1338df16b90b09afc9915400821189995d
TomatoFT <20520926@gm.uit.edu.vn>
2024-05-19 01:07:08 +0700

    KAN-27: Rewrite the login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  authService: AuthService;
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  showSidebar: boolean = true;
 
-  constructor(authService: AuthService) {
-    this.authService = authService;
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Check if the current URL is the root URL
+        this.showSidebar = event.url !== '/' && event.urlAfterRedirects !== '/';
+      }
+    });
   }
 }
+
+
